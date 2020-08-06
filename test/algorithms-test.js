46477232393859ff3ba829508112d88d26ef32f0
Sam McCall <smccall@smccall.com>
2020-08-06 13:57:25 -0400

    Reorganize file structure

--- test/algorithms-test.js
+++ test/algorithms-test.js
@@ -1,14 +1,14 @@
 let expect = require('chai').expect;
-let multiply = require('../algorithms/algorithms');
+let multiplier = require('../multiplier/multiplier');
 
 describe("Multiplication Algorithms", () => {
   describe("Method: Traditional", () => {
     describe("Helper functions", () => {
       it("Converts a Number into an array of its digits", () => {
-        let singleDigit = multiply.convertNumberToArray(4);
-        let singleDigitAgain = multiply.convertNumberToArray(0);
-        let multiDigit = multiply.convertNumberToArray(95);
-        let bigNumber = multiply.convertNumberToArray(189364872);
+        let singleDigit = multiplier.convertNumberToArray(4);
+        let singleDigitAgain = multiplier.convertNumberToArray(0);
+        let multiDigit = multiplier.convertNumberToArray(95);
+        let bigNumber = multiplier.convertNumberToArray(189364872);
 
         expect(JSON.stringify(singleDigit)).to.equal(JSON.stringify([4]));
         expect(JSON.stringify(singleDigitAgain)).to.equal(JSON.stringify([0]));
@@ -18,17 +18,17 @@ describe("Multiplication Algorithms", () => {
       });
 
       it("Converts an array of digits into a Number", () => {
-        let singleItemArray = multiply.convertArrayToNumber([4]);
-        let multiItemArray = multiply.convertArrayToNumber([9, 5, 4, 6]);
-        let bigArray = multiply.convertArrayToNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+        let singleItemArray = multiplier.convertArrayToNumber([4]);
+        let multiItemArray = multiplier.convertArrayToNumber([9, 5, 4, 6]);
+        let bigArray = multiplier.convertArrayToNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
 
         expect(singleItemArray).to.equal(4);
         expect(multiItemArray).to.equal(9546);
         expect(bigArray).to.equal(1234567890);
       });
       it("Determines if a number is negative", () => {
-        let negNumber = multiply.isNegative(-4);
-        let posNumber = multiply.isNegative(4);
+        let negNumber = multiplier.isNegative(-4);
+        let posNumber = multiplier.isNegative(4);
 
         expect(negNumber).to.equal(true);
         expect(posNumber).to.equal(false);
@@ -37,24 +37,24 @@ describe("Multiplication Algorithms", () => {
     describe.only("Single digit multiplication", () => {
 
       it("Multiplies single digit positive numbers", () => {
-        let singlePositiveDigits = multiply.traditionalMethod(3, 4);
+        let singlePositiveDigits = multiplier.traditionalMethod(3, 4);
         expect(singlePositiveDigits).to.equal(12);
       });
 
       it("Multiplies single digit negative numbers", () => {
-        let singleNegativeDigits = multiply.traditionalMethod(-5, -9);
+        let singleNegativeDigits = multiplier.traditionalMethod(-5, -9);
         expect(singleNegativeDigits).to.equal(45);
       });
 
       it("Multiplies single digit positive and negative numbers", () => {
-        let mixedSingleDigits = multiply.traditionalMethod(-1, 8);
+        let mixedSingleDigits = multiplier.traditionalMethod(-1, 8);
         expect(mixedSingleDigits).to.equal(-8);
       });
     });
-    
+
     describe("Multi-digit multiplication", () => {
       it("Multiplies positive 2-digit numbers", () => {
-        let twoDigitNumbers = multiply.traditionalMethod(10, 12);
+        let twoDigitNumbers = multiplier.traditionalMethod(10, 12);
         expect(twoDigitNumbers).to.equal(120);
       });
     });
