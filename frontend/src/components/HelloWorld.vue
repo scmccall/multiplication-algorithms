097cd140688222e554b6a99fca5b74f577fdb263
Sam McCall <smccall@smccall.com>
2020-08-23 16:28:09 -0400

    Add basic frontend logic to listen to backend

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,39 +1,49 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-mocha" target="_blank" rel="noopener">unit-mocha</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <p>{{ traditionalValue }} || {{ traditionalTime }}</p>
+    <p>{{ karatsubaValue }} || {{ karatsubaTime }}</p>
+    <button @click="multiplyButton(3, 4)">Multiply 3 * 4</button>
+    <div>
+      {{ traditionalRes }}
+      {{ karatsubaRes }}
+    </div>
   </div>
 </template>
 
 <script>
+
+import multiply from "../../../backend/assets/timer"
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data : function() {
+    return {
+      traditionalValue : 0,
+      traditionalTime : 0,
+      karatsubaValue : 0,
+      karatsubaTime : 0,
+
+      traditionalRes : null,
+      karatsubaRes : null,
+    }
+  },
+  methods : {
+    multiplyButton : function(a, b) {
+      let traditionalResult = multiply.multiplyWithTimer(a, b, 'traditional');
+      let karatsubaResult = multiply.multiplyWithTimer(a, b, 'karatsuba');
+      console.log('hello');
+
+      this.traditionalRes = traditionalResult;
+      this.karatsubaRes = karatsubaResult;
+
+      this.traditionalValue = traditionalResult[0];
+      this.traditionalTime = traditionalResult[1];
+      this.karatsubaValue = karatsubaResult[0];
+      this.karatsubaTime = karatsubaResult[1];
+    }
   }
 }
 </script>
